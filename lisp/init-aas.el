9212b2b3f45d02765c6c580f93b6a0652b28253b
yc wang <ywangmy@connect.ust.hk>
2022-12-31 15:24:53 +0800

    Sat Dec 31 15:24:53 HKT 2022

--- lisp/init-aas.el
+++ lisp/init-aas.el
@@ -1,51 +1,51 @@
 ;; aas and laas
-(use-package laas
-  :ensure t
-  :hook ((LaTeX-mode . laas-mode)
-         (org-mode . laas-mode)
-         )
-  :init
-  (setq laas-accent-snippets nil)
-  (setq laas-basic-snippets nil)
-  ;;(setq laas-subscript-snippets nil)
-  :custom
-  (laas-enable-auto-space nil)
-  :config
-  (aas-set-snippets 'laas-mode
-    "ii" nil
-    "ip1" nil
-    "jj" nil
-    "jp1" nil
-    "kk" nil
-    "kp1" nil
-    "nn" nil
-    "np1" nil
-    )
-  (aas-set-snippets 'laas-mode
-    ;; operators
-    :cond #'texmathp
-    "n->" "\\nrightarrow "
-    "n=>" "\\nRightarrow "
-    ;; symbols
-    :cond #'texmathp
-    ;; Calculus
-    "eva" (lambda () (interactive) (yas-expand-snippet "\\bigg\\rvert_{${1:a}}^{${2:b}}"))
-    ;; functions & symbols
-    :cond #'texmathp
-    "inv" "^{-1}"
-    "lisu" (lambda () (interactive) (yas-expand-snippet "\\limsup\\limits_{${1:n} \\to ${2:\\infty}} $0"))
-    "liin" (lambda () (interactive) (yas-expand-snippet "\\liminf\\limits_{${1:n} \\to ${2:\\infty}} $0"))
-    "oint" (lambda () (interactive) (yas-expand-snippet "\\oint_{${1:C}} $0"))
-    "curl" "\\nabla\\times "
-    "dive" "\\nabla\\cdot "
-    "var" "\\sigma^2"
-    ;; wrappers
-    :cond #'texmathp
-    "abs" (lambda () (interactive) (yas-expand-snippet "\\left\\lvert $0 \\right\\rvert"))
-    "vv" (lambda () (interactive) (yas-expand-snippet "\\vv{$0}"))
-    "hh" (lambda () (interactive) (yas-expand-snippet "\\hat{$0}"))
-    :cond #'laas-object-on-left-condition
-    )
-  )
+;; (use-package laas
+;;   :ensure t
+;;   :hook ((LaTeX-mode . laas-mode)
+;;          (org-mode . laas-mode)
+;;          )
+;;   :init
+;;   (setq laas-accent-snippets nil)
+;;   (setq laas-basic-snippets nil)
+;;   ;;(setq laas-subscript-snippets nil)
+;;   :custom
+;;   (laas-enable-auto-space nil)
+;;   :config
+;;   (aas-set-snippets 'laas-mode
+;;     "ii" nil
+;;     "ip1" nil
+;;     "jj" nil
+;;     "jp1" nil
+;;     "kk" nil
+;;     "kp1" nil
+;;     "nn" nil
+;;     "np1" nil
+;;     )
+;;   (aas-set-snippets 'laas-mode
+;;     ;; operators
+;;     :cond #'texmathp
+;;     "n->" "\\nrightarrow "
+;;     "n=>" "\\nRightarrow "
+;;     ;; symbols
+;;     :cond #'texmathp
+;;     ;; Calculus
+;;     "eva" (lambda () (interactive) (yas-expand-snippet "\\bigg\\rvert_{${1:a}}^{${2:b}}"))
+;;     ;; functions & symbols
+;;     :cond #'texmathp
+;;     "inv" "^{-1}"
+;;     "lisu" (lambda () (interactive) (yas-expand-snippet "\\limsup\\limits_{${1:n} \\to ${2:\\infty}} $0"))
+;;     "liin" (lambda () (interactive) (yas-expand-snippet "\\liminf\\limits_{${1:n} \\to ${2:\\infty}} $0"))
+;;     "oint" (lambda () (interactive) (yas-expand-snippet "\\oint_{${1:C}} $0"))
+;;     "curl" "\\nabla\\times "
+;;     "dive" "\\nabla\\cdot "
+;;     "var" "\\sigma^2"
+;;     ;; wrappers
+;;     :cond #'texmathp
+;;     "abs" (lambda () (interactive) (yas-expand-snippet "\\left\\lvert $0 \\right\\rvert"))
+;;     "vv" (lambda () (interactive) (yas-expand-snippet "\\vv{$0}"))
+;;     "hh" (lambda () (interactive) (yas-expand-snippet "\\hat{$0}"))
+;;     :cond #'laas-object-on-left-condition
+;;     )
+;;   )
 
 (provide 'init-aas)
--- lisp/init-yasnippet.el
+++ lisp/init-yasnippet.el
@@ -1,39 +1,19 @@
 ;; YASnippet
-(defun my-selected-text ()
-  "mark word at point"
-  (when (not (region-active-p))
-    (let* ((start (point)) (end (point))
-           (syntaxes "w_") ;; "w" for real word
-           )
-      (skip-syntax-backward syntaxes)
-      (setq start (point))
-      (skip-syntax-forward syntaxes)
-      (setq end (point))
-      (unless (= start end)
-        (goto-char start)
-        (set-mark-command ())
-        (goto-char end)
-        )))
-  nil
+
+(use-package yasupp
+  :hook
+  (yas-minor-mode . yasp-minor-mode)
   )
 
 (use-package yasnippet
   :ensure t
   :init
   (yas-global-mode 1)
-  :hook
-  (yasnippet-mode . #'my-selected-text)
   :custom
   (yas-snippet-dirs '("~/.emacs.d/yasnippets"))
   (yas-prompt-functions '(yas-dropdown-prompt))
   (yas-wrap-around-region nil)
   (yas-triggers-in-field t)
   )
-
-(defun my-yas-try-expanding-auto-snippets ()
-  (when yas-minor-mode
-    (let ((yas-buffer-local-condition ''(require-snippet-condition . auto)))
-      (yas-expand))))
-(add-hook 'post-command-hook #'my-yas-try-expanding-auto-snippets)
 
 (provide 'init-yasnippet)
